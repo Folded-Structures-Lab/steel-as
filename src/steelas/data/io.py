6b845b04c3240773ed52cf39f01d4cc224d7293d
joegattas <j.gattas@uq.edu.au>
2024-03-13 21:07:03 +1000

    v0.1.2
    added data csvs, fixed example_1

--- src/steelas/data/io.py
+++ src/steelas/data/io.py
@@ -1,8 +1,10 @@
-
 import os
 import pandas as pd
 
-def import_section_library(filename: str, skiprows: int | list[int] | None = [1]) -> pd.DataFrame:
+
+def import_section_library(
+    filename: str, skiprows: int | list[int] | None = [1]
+) -> pd.DataFrame:
     """
     Imports a section library from a CSV file located at 'steelas/data/{filename}.csv'.
 
@@ -12,8 +14,8 @@ def import_section_library(filename: str, skiprows: int | list[int] | None = [1]
     Raises:
         FileNotFoundError: If the CSV file does not exist.
     """
-    cwd = os.path.dirname(__file__) 
-    dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
-    lib_path = os.path.join(dir, f'data/{filename}.csv')
-    #return csv without unit row
+    cwd = os.path.dirname(__file__)
+    # dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    lib_path = os.path.join(cwd, f"{filename}.csv")
+    # return csv without unit row
     return pd.read_csv(lib_path, skiprows=skiprows)
